0eb415942edb58bc507c508f7de158cfc9edd75a
Darkarms <Steveo9001@hotmail.com>
2026-03-17 15:41:47 -0400

    fix to use specific urls for home and subscriptions page to avoid deleting videos in peoples channel pages

--- Scripts/Remove_Shorts.js
+++ Scripts/Remove_Shorts.js
@@ -1,6 +1,12 @@
+const YOUTUBE_MAIN_URL = "https://www.youtube.com/";
+const YOUTUBE_SUBSCRIPTIONS_URL = "https://www.youtube.com/feed/subscriptions";
+
 async function execute() { 
     while(true) {
-        remove_ID("dismissible");
+        let page_url = window.location.href;
+        if(page_url == YOUTUBE_MAIN_URL || page_url == YOUTUBE_SUBSCRIPTIONS_URL) {
+            remove_ID("dismissible");
+        }
         await sleep(100);
     }
 }
