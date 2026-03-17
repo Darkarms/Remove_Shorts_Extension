412380ab9a94429ffc33e8ab31fe22de4015160a
Darkarms <Steveo9001@hotmail.com>
2026-03-17 11:53:37 -0400

    fix async issue causing extension to not sleep

--- Scripts/Remove_Shorts.js
+++ Scripts/Remove_Shorts.js
@@ -1,13 +1,17 @@
-while(true) {
-    remove_ID("dismissible")
-    sleep(100)
+async function execute() { 
+    while(true) {
+        remove_ID("dismissible");
+        await sleep(100);
+    }
 }
 
+execute();
+
 function sleep(ms) { 
-    return new Promise(resolve => setTimeout(resolve, ms))
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function remove_ID(ID) { 
-    let Shorts_Container = document.getElementById(ID)
-    Shorts_Container.remove()
+    let Shorts_Container = document.getElementById(ID);
+    Shorts_Container.remove();
 }
