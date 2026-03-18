6b003c6c961fbda33b918a4f47240b9a50a66c9e
Darkarms <Steveo9001@hotmail.com>
2026-03-17 21:18:42 -0400

    add functionality for search results removal of most shorts

--- Scripts/Remove_Shorts.js
+++ Scripts/Remove_Shorts.js
@@ -1,11 +1,15 @@
 const YOUTUBE_MAIN_URL = "https://www.youtube.com/";
 const YOUTUBE_SUBSCRIPTIONS_URL = "https://www.youtube.com/feed/subscriptions";
+const YOUTUBE_SEARCH_URL = "https://www.youtube.com/results?search_query="
 
 async function execute() { 
     while(true) {
         let page_url = window.location.href;
         if(page_url == YOUTUBE_MAIN_URL || page_url == YOUTUBE_SUBSCRIPTIONS_URL) {
             remove_ID("dismissible");
+        }
+        if(page_url.includes(YOUTUBE_SEARCH_URL)) {
+            remove_class("ytGridShelfViewModelHost")
         }
         await sleep(100);
     }
@@ -18,8 +22,15 @@ function sleep(ms) {
 }
 
 function remove_ID(ID) { 
-    let Shorts_Container = document.getElementById(ID)
-    if (Shorts_Container) { 
-        Shorts_Container.remove();
+    let id_container = document.getElementById(ID);
+    if (id_container) { 
+        id_container.remove();
     }
+}
+
+function remove_class(element_class) { 
+    let class_container = document.getElementsByClassName(element_class);
+    if (class_container[0]) { 
+            class_container[0].remove();
+        }
 }
